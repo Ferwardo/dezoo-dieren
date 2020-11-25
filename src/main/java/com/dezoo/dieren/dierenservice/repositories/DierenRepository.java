ff990c6e884b4087e660d29a7eb2e35ccbd1f00b
CNeefs <r0764900@student.thomasmore.be>
2020-11-25 15:45:16 +0100

    added get endpoints in repository and controller

--- src/main/java/com/dezoo/dieren/dierenservice/repositories/DierenRepository.java
+++ src/main/java/com/dezoo/dieren/dierenservice/repositories/DierenRepository.java
@@ -2,6 +2,10 @@ package com.dezoo.dieren.dierenservice.repositories;
 import com.dezoo.dieren.dierenservice.models.DierModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DierenRepository extends JpaRepository<DierModel, Integer> {
     DierModel findDierModelByAnimalId(String animalId);
+    List<DierModel> findDierModelByVertebrate();
+    List<DierModel> findDierModelByClassification(String classification);
 }
