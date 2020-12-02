cf1fed9c4d4061a41ede5a360e977eb469ae305f
CNeefs <r0764900@student.thomasmore.be>
2020-12-02 14:33:53 +0100

    added more integration tests

--- src/test/java/com/dezoo/dieren/dierenservice/DierenControllerIntegrationTests.java
+++ src/test/java/com/dezoo/dieren/dierenservice/DierenControllerIntegrationTests.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Date;
@@ -79,8 +80,67 @@ public class DierenControllerIntegrationTests {
                 .andExpect(jsonPath("[0].dateOfBirth", is("1998-05-11T00:00:00.000+00:00")))
                 .andExpect(jsonPath("[0].vertebrate", is("true")))
                 .andExpect(jsonPath("[0].classification", is("Bird")));
+    }
 
+    @Test
+    public void givenDier_whenGetDierByID_ThenReturnJsonDier() throws Exception{
+        mockMvc.perform(get("/dieren/animalId", "lion1"))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.animalId", is("lion1")))
+                .andExpect(jsonPath("$.Name", is("Simba")))
+                .andExpect(jsonPath("$.kind", is("Lion")))
+                .andExpect(jsonPath("$.dateOfBirth", is("2003-03-03T00:00:00.000+00:00")))
+                .andExpect(jsonPath("$.vertebrate", is("true")))
+                .andExpect(jsonPath("$.classification", is("Mammal")));
+    }
+    
+    @Test
+    public void whenPostDier_ThenReturnJsonDier() throws Exception{
+        DierModel DierModel3 = new DierModel("stickbug1", "Sticker", "Stickbug", format.parse("29/11/2020"), false, "Insect");
+        
+        mockMvc.perform(post("/animals")
+                .content(mapper.writeValueAsString(DierModel3))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.animalId", is("stickbug1")))
+                .andExpect(jsonPath("$.Name", is("Sticker")))
+                .andExpect(jsonPath("$.kind", is("Stickbug")))
+                .andExpect(jsonPath("$.dateOfBirth", is("2020-11-29T00:00:00.000+00:00")))
+                .andExpect(jsonPath("$.vertebrate", is("false")))
+                .andExpect(jsonPath("$.classification", is("Insect")));
+                
+    }
 
+    @Test
+    public void givenDier_whenPutDier_ThenReturnJsonDier() throws Exception{
+        DierModel updateDierModel = new DierModel("lion1", "Mufasa", "Lion", format.parse("03/03/2003"), true, "Mammal");
 
+        mockMvc.perform(put("/animals")
+                .content(mapper.writeValueAsString(updateDierModel))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.animalId", is("lion1")))
+                .andExpect(jsonPath("$.Name", is("Mufasa")))
+                .andExpect(jsonPath("$.kind", is("Lion")))
+                .andExpect(jsonPath("$.dateOfBirth", is("2003-03-03T00:00:00.000+00:00")))
+                .andExpect(jsonPath("$.vertebrate", is("true")))
+                .andExpect(jsonPath("$.classification", is("Mammal")));
+    }
+
+    @Test
+    public void givenDier_WhenDeleteDier_ThenStatusOk() throws Exception{
+        mockMvc.perform(delete("/animals/{animalId}", "parrot1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void givenNoDier_whenDeleteDier_ThenStatusNotFound() throws Exception{
+        mockMvc.perform(delete("/animals/{animalId}", "parrot1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
     }
 }
